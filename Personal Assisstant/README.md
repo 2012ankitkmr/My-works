16657544ad911605be7dcf788c631339ebc873f7
Ankit Kumar <2012ankitkmr@gmail.com>
2016-10-02 16:54:27 +0530

    new Project Started

---
+++ Personal Assisstant/README.md
@@ -0,0 +1,11 @@
+Open Source Personal Assistant Proejct 
+
+Jarvis currently supports : 
+  - weather 
+  - squaring numbers
+  
+TODO:
+  - multiple interactions
+  
+
+
---
+++ Personal Assisstant/garud.py
@@ -0,0 +1,80 @@
+import sys
+import requests 
+import json
+import apis
+
+def square(n):
+    """ Square numbers  
+
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    """
+    return n*n
+
+def dispatcher(command, arg):
+    """ Does things """
+    if command == "weather":
+        print("Here's the weather forcast for "+arg)
+        print(apis.fetch_weather(arg))
+    if command == "square":
+        print("The square of " + arg + " is " + str(square(int(arg))))
+    elif command == "go away":
+        print("It sounds like you no longer need my assistance")
+        print("Very well. Goodbye!")
+        return 
+    elif command == "bye":
+        print("Goodbye! Have a good day!")
+        return
+    # Reprompt the user. 
+    prompter()
+
+
+def prompter():
+    """ asks for things """
+
+    command = (input("How may I help you?: [weather, square, go away, bye, dance]")).lower()
+    if command == "weather":
+        city = input("Sure thing! What city?")
+        dispatcher(command, city)
+    if command == "square":
+        num = input("I love math! What number?")
+        dispatcher(command, num)
+    if command == "stocks": # TODO
+        pass
+    elif command == "dance":
+        dance()
+    else:
+        dispatcher(command, "")
+
+def starter(cliargs):
+    # TODO: Finish up command line interface
+    print("DEBUG: Called with ", cliargs[1:])
+
+    print("Summoning Garud")
+    print("Good afternoon Ankit!")
+
+    if (len(cliargs) > 1):
+        command = cliargs[1]
+        arg = cliargs[2]
+        # TODO: Call dispatcher with args instead of prompting user. 
+        dispatcher(command, arg)
+    else:
+        prompter()
+
+#this is a comment
+def dance():
+    """Every personal assistant should know how to dance!
+    TODO: Add more dances
+
+    >>> dance()
+    left right chachacha left right chacha
+    left left right right dip up chachacha
+
+    """
+    print("left right chachacha left right chacha\nleft left right right dip up chachacha")
+    return
+
+if __name__ == "__main__":
+    starter(sys.argv)
